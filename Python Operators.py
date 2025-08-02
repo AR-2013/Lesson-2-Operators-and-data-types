0c87a1d9605aebb7ef9fff050629d8261401f0e8
AR-2013 <anairarandeva@icloud.com>
2025-08-02 11:48:44 +0400

    Python Operators

---
+++ Python Operators.py
@@ -0,0 +1,10 @@
+num1 = 2
+num2 = 13
+
+print ("Number 1", num1)
+print ("Number 2", num2)
+print ("Addition :", num1+num2)
+print ("Difference :", num1-num2)
+print ("Product :", num1*num2)
+print ("Division :", num1/num2)
+print ("Floor Division :", num1//num2)
